3942348ad6546b015fa4995cd96b34e643fde43e
NishantUpadhyay-BTC <nishantupadhyay@botreeconsulting.com>
2017-08-03 15:25:09 +0530

    Remove unnecessary code

--- app/controllers/v1/users/sessions_controller.rb
+++ app/controllers/v1/users/sessions_controller.rb
@@ -1,25 +1,15 @@
-class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+module V1
+  class Users::SessionsController < Devise::SessionsController
+    respond_to :json
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    # POST /resource/sign_in
+    def create
+      self.resource = warden.authenticate!(auth_options)
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      respond_to do |format|
+        format.json { render json: { data: { user: self.resource }}, status: 200 }
+      end
+    end
+  end
 end
