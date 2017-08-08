35ecaa99ba86c2681d1bda104b770cdb688601db
NaiyaShah-BTC <naiya.shah@botreetechnologies.com>
2017-08-08 17:48:04 +0530

    add carrierwave for avatar upaload in user profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808072123) do
+ActiveRecord::Schema.define(version: 20170808120037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170808072123) do
     t.string "last_name"
     t.float "latitude"
     t.float "longitude"
+    t.string "avatar"
     t.index ["latitude"], name: "index_profiles_on_latitude"
     t.index ["longitude"], name: "index_profiles_on_longitude"
   end
