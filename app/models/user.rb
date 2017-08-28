87058afcb35d5f4a9127b0492680f18b2a0ad3f3
NaiyaShah-BTC <naiya.shah@botreetechnologies.com>
2017-08-28 19:28:36 +0530

    changes in user email indexing

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
                                   foreign_key: 'follower_id',
                                   dependent:   :destroy
   has_many :favorites, through: :active_relationships, source: :followed
+  validates_uniqueness_of :uid
   validates_presence_of   :email, if: :email_required?
   validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
   validates_uniqueness_of :email, allow_blank: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170818121615) do
+ActiveRecord::Schema.define(version: 20170828135248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,8 +76,8 @@ ActiveRecord::Schema.define(version: 20170818121615) do
     t.string "uid"
     t.string "platform"
     t.string "device_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
 end
