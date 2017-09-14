d80cffe6f1b4795af9621a3ee77f9091cedb08e4
NishantUpadhyay-BTC <nishantupadhyay@botreeconsulting.com>
2017-09-14 20:06:38 +0530

    remove and change name

--- app/constraints/api_constraint.rb
+++ app/constraints/api_constraint.rb
@@ -6,8 +6,6 @@ class ApiConstraint
   end
 
   def matches?(request)
-    Rails.logger.info "request Headers >>>>>>>> #{request.headers.fetch(:version1)}"
-    Rails.logger.info "request Headers >>>>>>>> #{request.headers.fetch("Authorization")}"
     return true if request.url.match(/terms_of_use|data_protection/)
     request.headers.fetch(:version1).split(', ').last.eql?(version1.to_s)
   end
